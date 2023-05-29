57a8f5d15c8f6eb5e37433edeb21df2f12beaba5
baiki206 <baikijmukhadze@gmail.com>
2023-05-29 19:45:29 +0400

    1 dan 100 mde

---
+++ anasdavaleba.lua
@@ -0,0 +1,200 @@
+q="1"
+qq="2"
+qqq="3"
+qqqq="4"
+qqqqq="5"
+qqqqqq="6"
+qqqqqqq="7"
+qqqqqqqq="8"
+qqqqqqqqq="9"
+qqqqqqqqqq="10"
+r="11"
+rr="12"
+rrr="13"
+rrrr="14"
+rrrrr="15"
+rrrrrr="16"
+rrrrrrr="17"
+rrrrrrrr="18"
+rrrrrrrrr="19"
+rrrrrrrrrr="20"
+t="21"
+tt="22"
+ttt="23"
+tttt="24"
+ttttt="25"
+tttttt="26"
+ttttttt="27"
+tttttttt="28"
+ttttttttt="29"
+tttttttttt="30"
+s="31"
+ss="32"
+sss="33"
+ssss="34"
+sssss="35"
+ssssss="36"
+sssssss="37"
+ssssssss="38"
+sssssssss="39"
+ssssssssss="40"
+y="41"
+yy="42"
+yyy="43"
+yyyy="44"
+yyyyy="45"
+yyyyyy="46"
+yyyyyyy="47"
+yyyyyyyy="48"
+yyyyyyyyy="49"
+yyyyyyyyyy="50"
+f="51"
+ff="52"
+fff="53"
+ffff="54"
+fffff="55"
+ffffff="56"
+fffffff="57"
+ffffffff="58"
+fffffffff="59"
+ffffffffff="60"
+h="61"
+hh="62"
+hhh="63"
+hhhh="64"
+hhhhh="65"
+hhhhhh="66"
+hhhhhhh="67"
+hhhhhhhh="68"
+hhhhhhhhh="69"
+hhhhhhhhhh="70"
+j="71"
+jj="72"
+jjj="73"
+jjjj="74"
+jjjjj="75"
+jjjjjj="76"
+jjjjjjj="77"
+jjjjjjjj="78"
+jjjjjjjjj="79"
+jjjjjjjjjj="80"
+k="81"
+kk="82"
+kkk="83"
+kkkk="84"
+kkkkk="85"
+kkkkkk="86"
+kkkkkkk="87"
+kkkkkkkk="88"
+kkkkkkkkk="89"
+kkkkkkkkkk="90"
+l="91"
+ll="92"
+lll="93"
+llll="94"
+lllll="95"
+llllll="96"
+lllllll="97"
+llllllll="98"
+lllllllll="99"
+llllllllll="100"
+print (q)
+print (qq)
+print (qqq)
+print (qqqq)
+print (qqqqq)
+print (qqqqqq)
+print (qqqqqqq)
+print (qqqqqqqq)
+print (qqqqqqqqq)
+print (qqqqqqqqqq)
+print (r)
+print (rr)
+print (rrr)
+print (rrrr)
+print (rrrrr)
+print (rrrrrr)
+print (rrrrrrr)
+print (rrrrrrrr)
+print (rrrrrrrrr)
+print (rrrrrrrrrr)
+print (t)
+print (tt)
+print (ttt)
+print (tttt)
+print (ttttt)
+print (tttttt)
+print (ttttttt)
+print (tttttttt)
+print (ttttttttt)
+print (tttttttttt)
+print (s)
+print (ss)
+print (sss)
+print (ssss)
+print (sssss)
+print (ssssss)
+print (sssssss)
+print (ssssssss)
+print (sssssssss)
+print (ssssssssss)
+print (y)
+print (yy)
+print (yyy)
+print (yyyy)
+print (yyyyy)
+print (yyyyyy)
+print (yyyyyyy)
+print (yyyyyyyy)
+print (yyyyyyyyy)
+print (yyyyyyyyyy)
+print (f)
+print (ff)
+print (fff)
+print (ffff)
+print (fffff)
+print (ffffff)
+print (fffffff)
+print (ffffffff)
+print (fffffffff)
+print (ffffffffff)
+print (h)
+print (hh)
+print (hhh)
+print (hhhh)
+print (hhhhh)
+print (hhhhhh)
+print (hhhhhhh)
+print (hhhhhhhh)
+print (hhhhhhhhh)
+print (hhhhhhhhhh)
+print (j)
+print (jj)
+print (jjj)
+print (jjjj)
+print (jjjjj)
+print (jjjjjj)
+print (jjjjjjj)
+print (jjjjjjjj)
+print (jjjjjjjjj)
+print (jjjjjjjjjj)
+print (k)
+print (kk)
+print (kkk)
+print (kkkk)
+print (kkkkk)
+print (kkkkkk)
+print (kkkkkkk)
+print (kkkkkkkk)
+print (kkkkkkkkk)
+print (kkkkkkkkkk)
+print (l)
+print (ll)
+print (lll)
+print (llll)
+print (lllll)
+print (llllll)
+print (lllllll)
+print (llllllll)
+print (lllllllll)
+print (llllllllll)
